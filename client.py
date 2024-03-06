71de1e71d08f9df800e46ad072b9436aa5451aae
Your Name <you@example.com>
2024-03-06 19:37:10 +0800

    added test for client, but not working

--- client.py
+++ client.py
@@ -12,7 +12,7 @@ def recvall(sock, n):
         data.extend(packet)  
     return data  
   
-class LlamaServerAPI:  
+class LlamaServerAPI(object):  
     def __init__(self, host, port):  
         self.host = host  
         self.port = port  
@@ -43,15 +43,54 @@ class LlamaServerAPI:
   
         self.sock.close()  
   
-    def chat_completion(
-        self, 
-        messages, 
-        temperature=0.6, 
-        max_tokens=1024,
-        top_p=0.9, 
-        stream=False
-    ):
+    def tmp(self, messages, temperature=0.6, max_tokens=1024, top_p=0.9, stream=False):
         print(messages)
+
+        # prepare data to send to server
+        data = {
+            "messages": messages,
+            "temperature": temperature,
+            "max_tokens": max_tokens,
+            "top_p": top_p,
+            "stream": stream
+        }
+
+        return None
+
+        data_str = json.dumps(data)
+
+        # send the data to server
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
+        self.sock.connect((self.host, self.port))  
+        self._send_data(data_str)  
+
+        response = None
+
+        if stream:
+            while True:  
+                raw_msglen = recvall(self.sock, 4)  
+                if not raw_msglen:  
+                    break  
+                msglen = struct.unpack('>I', raw_msglen)[0]  
+                data = recvall(self.sock, msglen).decode()  
+                chunk_dict = json.loads(data)  
+
+                yield chunk_dict["delta"]  
+        else:
+            raw_msglen = recvall(self.sock, 4)  
+            msglen = struct.unpack('>I', raw_msglen)[0]  
+            data = recvall(self.sock, msglen).decode()  
+            response = json.loads(data)  
+  
+        self.sock.close()  
+
+        return response
+    
+
+    def chat_completion(self, messages, temperature=0.6, max_tokens=1024, top_p=0.9, stream=False):
+        print(messages)
+
+        return None
 
         # prepare data to send to server
         data = {
@@ -90,11 +129,10 @@ class LlamaServerAPI:
 
         return response
 
-
 def main():  
     host = 'msraig-ubuntu-2'  
     port = 65432  
-    llama = LlamaServerAPI(
+    llama_client = LlamaServerAPI(
         host=host, 
         port=port
     )  
@@ -109,7 +147,7 @@ def main():
         dialog.append({"role": "user", "content": input_text})
 
         try:
-            response = llama.chat_completion(
+            response = llama_client.tmp(
                 messages=dialog,                
                 temperature=0.6,
                 max_tokens=1024,
@@ -118,7 +156,7 @@ def main():
             )
 
             if stream_mode:
-                for chunk in llama.get_response_stream(input_text):  
+                for chunk in llama_client.get_response_stream(input_text):  
                     print(chunk, end=' ', flush=True)              
             else:
                 print(response['message']['content'])
